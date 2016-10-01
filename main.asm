18558ea2f0ebec0cdc0523a0fc7129a3d199625b
Mike Lang <mikelang3000@gmail.com>
2016-10-01 06:41:20 -0700

    Test data and loading

--- main.asm
+++ main.asm
@@ -1,5 +1,4 @@
 include "vram.asm"
-include "sprites.asm"
 
 
 section "Main", ROM0
@@ -9,41 +8,7 @@ Start::
 
 	; Set stack to top of internal RAM
 	ld SP, StackTop
-	ld HL, BaseTileMap ; test includes
-	
-	stop
-	call ClearScreen;
-	
-	
+
+	call LoadTestLevel
+
 	jp HaltForever
-
-	
-	
-ClearScreen::
-	; Set stack to top of internal RAM
-	ld SP, StackTop
-	
-	
-	
-	ld HL, BaseTileMap
-	ld B, 156 ;
-	;FOREACH TILE IN 156
-.forTile
-
-		ld C, 16
-		;FOREACH BYTE IN 16
-.forTileByte
-			;;Clear sprites
-			ld [HL], SpriteBackground
-			inc HL
-			ld [HL], SpriteBackground
-			inc HL
-			
-			dec C
-		jp nz, .forTileByte
-		dec B
-	jp nz, .forTile
-
-	;Every frame hack
-	stop
-	jp ClearScreen
