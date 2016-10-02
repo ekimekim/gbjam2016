096e944ede23790b02b8a22296551999671dfce5
Mike Lang <mikelang3000@gmail.com>
2016-10-02 02:57:11 -0700

    Make vblank copy actually copy the right source

--- vblank.asm
+++ vblank.asm
@@ -17,11 +17,29 @@ CopyWorkingGrid::
 	ld HL, SP+0
 	ld B, H
 	ld C, L ; save SP in BC
+
+	; for each count in PartNumber, we add 256 to the TileGrid, ie. 8 rows
+	; however, 8 rows in the WorkingGrid is only 8*20 = 160 = $a0
+	; so we need to set HL = base + n * $a0
 	ld A, [WorkingGridPartNumber]
-	add (WorkingGrid & $ff00) >> 8 ; get upper byte of WorkingGrid + part number
+	ld D, A
+	ld HL, WorkingGrid
+	; pass immediately if A = 0
+	and A
+	jr z, .afterpartloop
+.partloop
+	ld A, L
+	add $a0 ; possibly carry
+	ld L, A
+	ld A, H
+	adc $0 ; add if carry
 	ld H, A
-	ld L, $0
+	dec D
+	jr nz, .partloop
+.afterpartloop
 	ld SP, HL ; stack pointer = source
+
+	; since we're multiplying by $100 here, we can cheat by adding to upper byte
 	ld A, [WorkingGridPartNumber]
 	add (TileGrid & $ff00) >> 8 ; get upper byte of TileGrid + part number
 	ld H, A
